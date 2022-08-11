f4d8af7845a84e5d8d5c9f4e90f11787e1939b0d
Santiago Tarrio <tarrio.sant@gmail.com>
2022-08-11 13:47:56 -0300

    agregado carrousel(estilos y responsive en proceso)

---
+++ src/main.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        300: {
+          slidesPerView: 1,
+          spaceBetween: 5,
+          slidesPerGroup: 1,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+          slidesPerGroup: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+          slidesPerGroup: 3,
+        },}
+  });
