59a6aed171a05b9415b745759c3131abca51726f
Santiago Tarrio <tarrio.sant@gmail.com>
2022-08-11 16:49:37 -0300

    cambios responsive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 20,
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
